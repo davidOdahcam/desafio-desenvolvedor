30c1b0614212e2cc8b8a0ad81a3a423873683b91
David dos Santos Machado <david.s.machado99@gmail.com>
2024-11-06 00:06:30 -0300

    chore: setting up database mongodb

--- challenge-api/routes/api.php
+++ challenge-api/routes/api.php
@@ -8,10 +8,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
         return $request->user();
     });
-});
 
-Route::controller(UploadController::class)->prefix('uploads')->group(function () {
-    Route::get('/', 'listUploads');
-    Route::post('/', 'uploadFile');
-    Route::get('/search', 'searchFile');
+    Route::controller(UploadController::class)->prefix('uploads')->group(function () {
+        Route::get('/', 'listUploads');
+        Route::post('/', 'uploadFile');
+        Route::get('/search', 'searchFile');
+    });
 });
