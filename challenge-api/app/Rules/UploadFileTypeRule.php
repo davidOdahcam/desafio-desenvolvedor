63f894f99569480e2effc6cdec6adb90eb034bcb
David dos Santos Machado <david.s.machado99@gmail.com>
2024-11-06 01:55:21 -0300

    feat: creating rule to check mimetype

---
+++ challenge-api/app/Rules/UploadFileTypeRule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class UploadFileTypeRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $allowedExtensions = ['xlsx', 'csv', 'zip'];
+        $extension = strtolower($value->getClientOriginalExtension());
+
+        if (!in_array($extension, $allowedExtensions)) $fail('The file field must be a file of type: ' . implode(', ', $allowedExtensions));
+    }
+}
